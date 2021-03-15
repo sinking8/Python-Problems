f14da714f2f3002b588882614b3b1632c8e0810b
sinking123 <75163334+sinking123@users.noreply.github.com>
2021-03-16 00:00:20 +0530

    added unit digit desc

---
+++ unit_digit_desc.py
@@ -0,0 +1,66 @@
+'''
+Unit Digit Descending
+The program must accept two integers as the input. The program must sort the two integers in descending order based on their unit digit and print them as the output. If the unit digits are same then sort the two integers in descending order and print them as the output.
+
+INPUT:
+Input contains the numbers separated by a space
+
+OUTPUT:
+Display the numbers sorted according to their unit digit
+
+CONSTRAINTS:
+1<= numbers <= 10^3
+
+SAMPLE INPUT:
+57 102
+
+SAMPLE OUTPUT: 
+57 102
+
+EXPLANATION: 
+102 57 
+2>7 hence 102 and 57 are not swapped
+
+SAMPLE INPUT 1:
+275 15
+
+SAMPLE OUTPUT 1:
+275 15
+
+SAMPLE INPUT 2:
+123 532
+
+SAMPLE OUTPUT 2:
+123 532
+
+SAMPLE INPUT 3:
+235 239
+
+SAMPLE OUTPUT 3:
+239 235
+
+SAMPLE INPUT 4:
+12 21
+
+SAMPLE OUTPUT 4:
+12 21
+
+SAMPLE INPUT 5:
+5 6
+
+SAMPLE OUTPUT 5:
+6 5
+
+SOLUTION:
+a, b = map(str,input().split())
+if(int(a[-1])>=int(b[-1])):
+	print(a,b,sep=" ")
+else:
+	print(b,a,sep=" ")
+
+'''
+a, b = map(str,input().split())
+if(int(a[-1])>=int(b[-1])):
+	print(a,b,sep=" ")
+else:
+	print(b,a,sep=" ")
